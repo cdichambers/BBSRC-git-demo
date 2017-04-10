7b38cc352049269e767350f12228e9353c2664da
Chris <chambersc1@cardiff.ac.uk>
2017-04-10 10:15:49 +0100

    adding plot

--- somecode.R
+++ somecode.R
@@ -2,3 +2,8 @@
 df=read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/LskyetalPLOSONE.csv',
               header=TRUE,sep=',')
 head(df)
+# R code
+lm.result=lm(conspiracist_avg~age,data=df)
+summary(lm.result)
+# R code
+plot(df$age,df$conspiracist_avg)
